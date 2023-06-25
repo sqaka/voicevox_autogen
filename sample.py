2abd4a96b40821bc30cf9127fd934f5afe647e8f
sqaka <sq.akachang@gmail.com>
2023-06-25 23:41:29 +0900

    multiple line reading

--- sample.py
+++ sample.py
@@ -1,23 +1,29 @@
-import requests, simpleaudio, tempfile, json
+import requests
+import simpleaudio
+import tempfile
+import json
 
-host = "127.0.0.1"
-port = 50021
+HOST = "127.0.0.1"
+PORT = 50021
 
-params = (("text", "こんにちは、ずんだもんです。"), ("speaker", 3))
-# https://github.com/VOICEVOX/voicevox_resource/search?q=styleId
+with open('./data/sample.txt') as f:
+    for line in f:
+        params = (("text", f"{line}"), ("speaker", 22))
+        # https://puarts.com/?pid=1830
 
-response1 = requests.post(f"http://{host}:{port}/audio_query", params=params)
+        response1 = requests.post(
+            f"http://{HOST}:{PORT}/audio_query", params=params)
+        response2 = requests.post(
+            f"http://{HOST}:{PORT}/synthesis",
+            headers={"Content-Type": "application/json"},
+            params=params,
+            data=json.dumps(response1.json()),
+        )
 
-response2 = requests.post(
-    f"http://{host}:{port}/synthesis",
-    headers={"Content-Type": "application/json"},
-    params=params,
-    data=json.dumps(response1.json()),
-)
-
-with tempfile.TemporaryDirectory() as tmp:
-    with open(f"{tmp}/audi.wav", "wb") as f:
-        f.write(response2.content)
-        wav_obj = simpleaudio.WaveObject.from_wave_file(f"{tmp}/audi.wav")
-        play_obj = wav_obj.play()
-        play_obj.wait_done()
+        with tempfile.TemporaryDirectory() as tmp:
+            with open(f"{tmp}/audi.wav", "wb") as f:
+                f.write(response2.content)
+                wav_obj = simpleaudio.WaveObject.from_wave_file(
+                    f"{tmp}/audi.wav")
+                play_obj = wav_obj.play()
+                play_obj.wait_done()
